39dd2963c48d7d98cb99676dbc296b7e8cbedcea
Rodrigo Fontes <rodrigo.cruz@ifs.edu.br>
2024-04-05 14:58:40 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [precoAlcool, setPrecoAlcool] = useState();
+  const [precoGasolina, setPrecoGasolina] = useState();
+
+  function calcular() {
+    if (precoAlcool && precoGasolina) {
+      if (precoAlcool / precoGasolina < 0.7) {
+        alert(`Vale mais a pena abastecer com ÁLCOOL`);
+      } else {
+        alert(`Vale mais a pena abastecer com GASOLINA`);
+      }
+    } else {
+      alert('Preencha o preço do Álcool e da Gasolina!');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+    <div className="Container">
+
+      <header>
+        <img src={logo} className="Logo" alt="logo" />
       </header>
+
+      <main>
+        <h1>
+          Qual a melhor opção?
+        </h1>
+
+        <label>Álcool (preço por litro): </label>
+        <input
+          type="text"
+          name="Name"
+          value={precoAlcool}
+          onChange={e => setPrecoAlcool(e.target.value)}
+        />
+
+        <label>Gasolina (preço por litro): </label>
+        <input
+          type="text"
+          name="Name"
+          value={precoGasolina}
+          onChange={e => setPrecoGasolina(e.target.value)}
+        />
+
+        <button onClick={calcular}>Calcular</button>
+      </main>
+
     </div>
   );
 }
